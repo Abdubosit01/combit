fea5267006e314a407cae5798d00083e52e54b28
Abdubosit <abdubosit0115@gmail.com>
2025-11-20 15:31:24 +0500

    lol

---
+++ src/component/portfolio.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Portfolio() {
+  return (
+    <div>
+      <h1>Portfolio</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium
+        doloribus perferendis ipsa, eaque commodi iusto nulla numquam. Provident
+        quod in cum, sit qui exercitationem temporibus officiis rem, itaque
+        voluptatem doloribus aliquam laudantium ipsum enim dolor hic. Saepe quam
+        ratione possimus, mollitia doloremque exercitationem vel perspiciatis
+        magnam enim? Non modi exercitationem, porro nobis doloribus ut,
+        necessitatibus nostrum at beatae ad explicabo commodi officiis possimus
+        vero quae tempora. Voluptatum iure sequi veritatis voluptas dignissimos
+        aut reprehenderit, non doloremque debitis quod laborum beatae quasi
+        placeat totam cupiditate minima rerum fugiat sint vel porro accusantium
+        possimus? Ipsa, alias expedita accusantium deserunt laborum ullam
+        aperiam?
+      </p>
+    </div>
+  );
+}
